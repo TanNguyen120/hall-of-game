7c44d188c19b0425da8a635a35a3510c57e2f94f
Taan <tannguyen1220@gmail.com>
2023-02-10 08:15:21 +0700

    basic footer info

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,18 +3,8 @@ import { Grid } from '@mui/material';
 export default function Footer() {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <div>xs = 4</div>
-      </Grid>
-      <Grid item xs={4}>
-        <div> text </div>
-      </Grid>
-      <Grid item xs={4}>
-        <div>text</div>
-      </Grid>
-      <Grid item xs={8}>
-        <div>text</div>
-      </Grid>
+      <div>Project Hall Of Game 2023</div>
+      <div>Author: Nguyen Viet Tan</div>
     </Grid>
   );
 }
