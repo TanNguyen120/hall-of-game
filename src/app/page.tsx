c04c6d945a3f0aeb70db69e8dd1fe593496f1c51
Taan <tannguyen1220@gmail.com>
2023-02-13 13:42:26 +0700

    game card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@mui/system';
 import { Container, Divider, Grid, Typography } from '@mui/material';
 import SpecCard from '@/components/homeComponents/specComponent/specComponent';
 import GearCard from '@/components/homeComponents/gearComponent/gearComponent';
+import GameCard from '@/components/homeComponents/gameCard/gameCard';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -35,6 +36,36 @@ export default function Home() {
           {/* Pc Spec will change through time */}
           <SpecCard />
           <GearCard />
+        </Grid>
+
+        {/* ----------------------------------------------------------------------- */}
+        {/* All Game feature in the website */}
+        <Typography variant='h5' color={'secondary'} sx={{ mb: 4, mt: 4 }}>
+          Games List:
+        </Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Grid
+          container
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+          gap={5}
+        >
+          {/* Metroid Fusion */}
+          <GameCard
+            gameTile='Metroid Fusion'
+            shortDescription=' One of the best metroidvania Game on the gameboy advance'
+            genre={['platformer', 'retro', 'puzzle']}
+            imgSrc='/component_img/gameListBoxArt/metroidFusion.jpg'
+          />
+          {/* ----------------------------------------------------------------------- */}
+          {/* Zelda And The MinishCap */}
+          <GameCard
+            gameTile='The Legend Of Zelda:The Minish Cap'
+            shortDescription=' Second zelda game on the gameboy advance '
+            genre={['hack&slash', 'retro', 'puzzle']}
+            imgSrc='/component_img/gameListBoxArt/zeldaTheMinishCap.jpg'
+          />
         </Grid>
       </Container>
       {/* </div> */}
---
+++ src/components/homeComponents/gameCard/gameCard.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function GameCard({
+  gameTile,
+  shortDescription,
+  genre,
+  imgSrc,
+}: {
+  gameTile: string;
+  shortDescription: string;
+  genre: Array<string>;
+  imgSrc: string;
+}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component='img' height='340' image={imgSrc} alt={gameTile} />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            {gameTile}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {shortDescription}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          {genre.map((element) => element)}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
